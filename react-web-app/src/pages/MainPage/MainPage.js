d9a379da51b57a5476d041635d059e6abca569e7
PetryhanDmytro <petryhan.dmytro@student.uzhnu.edu.ua>
2024-11-02 15:37:31 +0200

    Adeded ProductPage

--- react-web-app/src/pages/MainPage/MainPage.js
+++ react-web-app/src/pages/MainPage/MainPage.js
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "../../components/Header";
 import Navigation from "../../components/Navigation";
 import HomePage from "../HomePage";
+import ProductPage from "../ProductPage";
 export default function MainPage() {
   return (
     <div className="home-page">
       <Header />
       <Navigation />
-      <HomePage />
+      {/* <HomePage /> */}
+      <ProductPage />
     </div>
   );
 }
